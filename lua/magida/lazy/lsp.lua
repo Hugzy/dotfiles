132189dadc55af494e7bd010a269cd10ce16e1eb
frederik madsen <frede.madsen3@gmail.com>
2024-07-03 16:16:57 +0200

    new omnisharp config still no autoimport

--- lua/magida/lazy/lsp.lua
+++ lua/magida/lazy/lsp.lua
@@ -65,45 +65,37 @@ return {
                 end,
 
                 ["omnisharp"] = function()
-                    local lspconfig = require("lspconfig")
-                    lspconfig.omnisharp.setup({
-                        handlers = {
+                    local server_opts = {
+                        enable_editorconfig_support = true,
+                        enable_roslyn_analyzers     = false,
+                        enable_import_completion    = true,
+                        sdk_include_prereleases     = false,
+                        handlers                    = {
                             ["textDocument/definition"] = require('omnisharp_extended').definition_handler,
                             ["textDocument/typeDefinition"] = require('omnisharp_extended').type_definition_handler,
                             ["textDocument/references"] = require('omnisharp_extended').references_handler,
                             ["textDocument/implementation"] = require('omnisharp_extended').implementation_handler,
-                        },
-                        capabilities = capabilities,
-                        cmd = { "dotnet", "/home/fhm/.local/share/nvim/mason/packages/omnisharp/libexec/OmniSharp.dll" },
-                        -- Enables support for reading code style, naming convention and analyzer
-                        -- settings from .editorconfig.
-                        enable_editorconfig_support = true,
-                        -- If true, MSBuild project system will only load projects for files that
-                        -- were opened in the editor. This setting is useful for big C# codebases
-                        -- and allows for faster initialization of code navigation features only
-                        -- for projects that are relevant to code that is being edited. With this
-                        -- setting enabled OmniSharp may load fewer projects and may thus display
-                        -- incomplete reference lists for symbols.
-                        enable_ms_build_load_projects_on_demand = false,
-                        -- Enables support for roslyn analyzers, code fixes and rulesets.
-                        enable_roslyn_analyzers = false,
-                        -- Specifies whether 'using' directives should be grouped and sorted during
-                        -- document formatting.
-                        organize_imports_on_format = true,
-                        -- Enables support for showing unimported types and unimported extension
-                        -- methods in completion lists. When committed, the appropriate using
-                        -- directive will be added at the top of the current file. This option can
-                        -- have a negative impact on initial completion responsiveness,
-                        -- particularly for the first few completion sessions after opening a
-                        -- solution.
-                        enable_import_completion = true,
-                        -- Specifies whether to include preview versions of the .NET SDK when
-                        -- determining which version to use for project loading.
-                        sdk_include_prereleases = true,
-                        -- Only run analyzers against open files when 'enableRoslynAnalyzers' is
-                        -- true
-                        analyze_open_documents_only = false,
+                        }
+                    }
+
+
+                    local capabilities_omnisharp = require("cmp_nvim_lsp").default_capabilities(vim.lsp.protocol.make_client_capabilities())
+                    capabilities.textDocument.completion.completionItem.snippetSupport = true
+                    capabilities.textDocument.foldingRange = {
+                        dynamicRegistration = true,
+                        lineFoldingOnly = true,
+                    }
+
+                    server_opts = vim.tbl_deep_extend("force", server_opts, {
+                        on_attach = on_attach,
+                        capabilities = capabilities_omnisharp,
+                        flags = {
+                            debounce_text_changes = 150,
+                        }
                     })
+
+                    local lspconfig = require("lspconfig")
+                    lspconfig.omnisharp.setup(server_opts)
                 end
             }
         })
